e7690d107a8bc10f0c6b9e5bc220a116078131f5
Nikita Sementsov <nikita.sementsov@zfx.com>
2026-08-22 21:10:37 +0200

    Added user greeting prompt to brain-games CLI
    Uses readline-sync to ask for the player's name so future games can
    address the user directly, rather than just printing a static welcome.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync'
+
 console.log('Welcome to the Brain Games!')
+console.log('May I have your name?')
+const user = readlineSync.question('Your answer: ');
+console.log(`Hello, ${user}`)
