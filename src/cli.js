82d370c6ccdf356ae5cecef8289ed17acb462da1
Nikita Sementsov <nikita.sementsov@zfx.com>
2026-08-26 22:55:47 +0200

    add even game

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,26 @@
 import readlineSync from 'readline-sync';
+import even from './even.js';
+
+const state = {
+  user: null,
+};
 
 function cli() {
   console.log('Welcome to the Brain Games!');
   console.log('May I have your name?');
   const user = readlineSync.question('Your answer: ');
-  console.log(`Hello, ${user}`);
+  state.user = user;
+  console.log(`Hello, ${state.user}`);
+  const result = selectGame()();
+  if (result.isSuccessful) {
+    console.log(`Congratulations, ${state.user}!`);
+  } else {
+    console.log(`Let's try again, ${state.user}!`);
+  }
+}
+
+function selectGame() {
+  return even;
 }
 
 export default cli;
---
+++ src/even.js
@@ -0,0 +1,18 @@
+import readlineSync from 'readline-sync';
+
+const EVEN_GAME_NUMBER_OF_QUESTIONS = 3;
+const EVEN_GAME_MAX_NUMBER = 1000;
+
+function even() {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let question = 0; question < EVEN_GAME_NUMBER_OF_QUESTIONS; question++) {
+    const number = Math.floor(Math.random() * EVEN_GAME_MAX_NUMBER);
+    console.log(`Question: ${number}`);
+    const expectedAnswer = number % 2 === 0 ? 'yes' : 'no';
+    const actualAnswer = readlineSync.question('Your answer: ').trim().toLowerCase();
+    if (expectedAnswer !== actualAnswer) return { isSuccessful: false };
+  }
+  return { isSuccessful: true };
+}
+
+export default even;
